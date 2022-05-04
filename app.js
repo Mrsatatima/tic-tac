1be41dc9aaf1d0e42a911c3fcf453af9e4c0466c
Al-amin Ahmad Abbas <aminuozil@gmail.com>
2022-05-04 15:16:09 +0100

    Add game win logic

--- app.js
+++ app.js
@@ -5,22 +5,52 @@ const playerTwoEditButton = document.querySelector("#player-2 button");
 const submitInputName = document.querySelector(".name-input #submit");
 const cancelInputName = document.querySelector(".name-input #cancel");
 const startGameButton = document.querySelector("#game-board button");
+const restartGameButton = document.querySelector(".end-game button");
 let playerOneTurn = true;
-let gameWin = false
-let gameDraw = false
-const boardBox = document.querySelector(".board") 
+let gameWin = false;
+let gameDraw = false;
+let nameofPlayerOne = "Player 1";
+let nameofPlayerTwo = "Player 2";
+const boardBox = document.querySelector(".board");
 
-const winStructure = [[0,1,2],[3,4,5],
-                      [6,7,8],[0,3,6],
-                      [1,4,7],[2,5,8],
-                      [0,4,8],[2,4,6]]
+const winStructure = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-function checkGameWin(){
-    for (const win of winStructure) {
-        if (boardBox.children[win[0]] && boardBox.children[win[1]] && boardBox.children[win[2]]){
-            gameWin = boardBox.children[win[0]].children[0]==boardBox.children[win[1]].children[0]==boardBox.children[win[2]].children[0]
-        }   
+function checkGameWin() {
+  for (const win of winStructure) {
+    if (
+      boardBox.children[win[0]].textContent &&
+      boardBox.children[win[1]].textContent &&
+      boardBox.children[win[2]].textContent
+    ) {
+      if (
+        boardBox.children[win[0]].children[0].textContent ==
+          boardBox.children[win[1]].children[0].textContent &&
+        boardBox.children[win[1]].children[0].textContent ==
+          boardBox.children[win[2]].children[0].textContent
+      ) {
+        gameWin = true;
+      }
     }
+  }
+}
+function checkDraw(){
+    const boardContents = document.querySelectorAll(".board li")
+    for (const box of boardContents) {
+        if (!box.textContent){
+            return false
+        }
+        
+    }
+    return true
 }
 
 function openInputNamePopup(event) {
@@ -56,8 +86,10 @@ function addPlayerName(event) {
     console.log(listOfClass);
     if (listOfClass.contains("player-1")) {
       playerOneName.textContent = playerName;
+      nameofPlayerOne =playerName
     } else {
       playerTwoName.textContent = playerName;
+      nameofPlayerTwo=playerName
     }
     inputNamePopup.classList.remove("display-popup", "player-1", "player-2");
     inputField.value = "";
@@ -66,43 +98,78 @@ function addPlayerName(event) {
 
 function startGame(event) {
   const targetButton = event.target;
-  console.dir(targetButton);
   const gameBoard = targetButton.nextElementSibling;
-  const listofBox = gameBoard.querySelectorAll("li")
-  for (const box of listofBox){
-      box.textContent =""
-      box.classList.remove("clicked-box")
+  const listofBox = gameBoard.querySelectorAll("li");
+  for (const box of listofBox) {
+    box.textContent = "";
+    box.classList.remove("clicked-box");
   }
   gameBoard.classList.add("display-gameboard");
+  playerOneTurn = true;
+  gameWin = false;
+  gameDraw = false;
 }
 
-function playerBoardInput(event){
-    boxClicked = event.target
-    const newPElement = document.createElement("p")
-    if (gameWin){
-        winDropDown = document.querySelector(".win")
-        winDropDown.classList.add("display-win")
+function restartGame(event) {
+    const targetButton = event.target;
+    const gameBoard = document.querySelector("#game-board");
+    const listofBox = gameBoard.querySelectorAll("li");
+    const grandParentElement = targetButton.parentElement.parentElement
+    for (const box of listofBox) {
+      box.textContent = "";
+      box.classList.remove("clicked-box");
     }
-    if (!boxClicked.textContent){
+    grandParentElement.classList.remove("display-win")
+    playerOneTurn = true;
+    gameWin = false;
+    gameDraw = false;
+  }
 
-        if (playerOneTurn){
-            newPElement.textContent = "X"
-            boxClicked.classList.add("clicked-box")
-            boxClicked.append(newPElement)
-            playerOneTurn = false
-        }else{
-            newPElement.textContent = "O"
-            boxClicked.classList.add("clicked-box")
-            boxClicked.append(newPElement)
-            playerOneTurn = true
-            
-        }
+function playerBoardInput(event) {
+  boxClicked = event.target;
+  const newPElement = document.createElement("p");
+
+  if (!boxClicked.textContent) {
+    if (playerOneTurn) {
+      newPElement.textContent = "X";
+      boxClicked.classList.add("clicked-box");
+      boxClicked.append(newPElement);
+      playerOneTurn = false;
+    } else {
+      newPElement.textContent = "O";
+      boxClicked.classList.add("clicked-box");
+      boxClicked.append(newPElement);
+      playerOneTurn = true;
     }
+  }
+  checkGameWin();
+  gameDraw = checkDraw()
+  if (gameWin) {
+    const winDropDown = document.querySelector(".win");
+    const winText = document.querySelector(".end-game h4");
+
+    if (playerOneTurn) {
+      let congratstext = nameofPlayerTwo + " wins";
+      winText.textContent = congratstext;
+    } else {
+      let congratstext = nameofPlayerOne + " wins";
+      winText.textContent = congratstext;
+    }
+    winDropDown.classList.add("display-win");
+  }
+  if (gameDraw){
+    const winDropDown = document.querySelector(".win");
+    const winText = document.querySelector(".end-game h4");
+    winText.textContent = congratstext;
+    winDropDown.classList.add("display-win");
+  }
 }
 playerOneEditButton.addEventListener("click", openInputNamePopup);
 playerTwoEditButton.addEventListener("click", openInputNamePopup);
 cancelInputName.addEventListener("click", closeInputNamePopup);
 submitInputName.addEventListener("click", addPlayerName);
 startGameButton.addEventListener("click", startGame);
+restartGameButton.addEventListener("click", restartGame);
 
-boardBox.addEventListener("click", playerBoardInput)
+
+boardBox.addEventListener("click", playerBoardInput);
